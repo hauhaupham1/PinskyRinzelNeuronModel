bafb4ae9c726b77e200b4cedb83850da387790e5
hauhaupham1 <hauhaupham1@gmail.com>
2025-04-25 23:07:45 -0500

    including Network

--- PRmodel_Motoneuron/__init__.py
+++ PRmodel_Motoneuron/__init__.py
@@ -1,13 +1,2 @@
-from .MotoneuronModel import MotoneuronModel, Network
-from .losses import (
-    load_target_values,
-    range_loss,
-    squared_error,
-    calculate_firing_rate,
-    calculate_rin,
-    calculate_spike_adaptation,
-    calculate_spike_amplitudes,
-    calculate_tau,
-    comprehensive_loss,
-    optimize_model
-)
+from .MotoneuronModel import MotoneuronModel
+from .Network import MotoneuronNetwork
